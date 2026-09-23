1f0ffc01a0506b2d92d902e5a2639c929c251300
unknown <ccpoveda.programador@gmail.com>
2026-09-23 09:45:36 -0500

    v 2.0.0

--- src/types/Product.ts
+++ src/types/Product.ts
@@ -1,12 +1,7 @@
 export interface Product {
   id: string;
   name: string;
-  price: number;
   images: string[];
   description: string;
-  category?: string;
-}
-
-export interface CartItem extends Product {
-  quantity: number;
+  estado?: boolean;
 }
